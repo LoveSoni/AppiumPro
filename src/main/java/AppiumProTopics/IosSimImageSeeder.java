902b3df7149b246df1fe5a6e9287e17b35bb8e68
love <lovesoni607@gmail.com>
2020-04-29 13:32:42 +0530

    push file to ios emulator

--- src/main/java/AppiumProTopics/IosSimImageSeeder.java
+++ src/main/java/AppiumProTopics/IosSimImageSeeder.java
@@ -2,19 +2,28 @@ package AppiumProTopics;
 
 import AppiumServer.Server;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 public class IosSimImageSeeder extends Server {
     private AppiumDriver appiumDriver;
+    private String PROJECT_PATH = System.getProperty("user.dir");
+    private String slash = File.separator;
 
     @BeforeMethod
     public void setUp(){
@@ -22,16 +31,28 @@ public class IosSimImageSeeder extends Server {
     }
 
     @Test
-    public void seedImage() throws MalformedURLException {
-        String slash = File.separator;
-        String appPath = System.getProperty("user.dir") + slash + "apps" + slash +"SamplePhotosApp.app";
+    public void seedImage() throws IOException {
+        String appPath = PROJECT_PATH + slash + "apps" + slash +"SamplePhotosApp.app";
+        File videoFile = new File(PROJECT_PATH + slash + "data"+ slash + "video.mp4");
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformVersion", "12.2");
         desiredCapabilities.setCapability("platformName", "iOS");
         desiredCapabilities.setCapability("deviceName", "iPhone 8 Plus");
         desiredCapabilities.setCapability("app",appPath);
         desiredCapabilities.setCapability("autoAcceptAlerts",true);
-        appiumDriver = new AndroidDriver(new URL(getServerUrl()),desiredCapabilities);
+        appiumDriver = new IOSDriver(new URL(getServerUrl()),desiredCapabilities);
+        WebDriverWait wait = new WebDriverWait(appiumDriver, 10);
+        WebElement el = wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("All Photos")));
+        el.click();
+        List<WebElement> photos = appiumDriver.findElements(MobileBy.className("XCUIElementTypeImage"));
+        int numPhotos = photos.size();
+        System.out.println("Number of Photos Available"+numPhotos);
+        System.out.println("Video File path "+videoFile.getCanonicalPath());
+        try {
+            ((IOSDriver)appiumDriver).pushFile("myVideo.mp4",videoFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
