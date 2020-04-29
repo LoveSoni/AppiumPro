e67fa7341747adcc88fcf237e860e6760334725c
love <lovesoni607@gmail.com>
2020-04-30 00:35:41 +0530

    execute shell commad using execute script

--- src/main/java/AppiumProTopics/ExecuteShellCommand.java
+++ src/main/java/AppiumProTopics/ExecuteShellCommand.java
@@ -9,59 +9,43 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Map;
 
-public class AndImageSeeder extends Server {
+public class ExecuteShellCommand extends Server {
     private AppiumDriver appiumDriver;
-    private String PROJECT_PATH = System.getProperty("user.dir");
-    private String slash = File.separator;
-
     @BeforeMethod
     public void setUp(){
         startServer();
     }
 
     @Test
-    public void androidSeeder(){
-        File imageFile = new File(PROJECT_PATH + slash + "data" + slash + "Image.jpeg");
+    public void androidSeeder() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "Android Emulator");
-        capabilities.setCapability(MobileCapabilityType.UDID,"emulator-5554");
+        capabilities.setCapability(MobileCapabilityType.UDID, "emulator-5554");
         capabilities.setCapability("automationName", "UiAutomator2");
         capabilities.setCapability("appPackage", "com.google.android.apps.photos");
         capabilities.setCapability("appActivity", ".home.HomeActivity");
-        capabilities.setCapability("autoAcceptAlerts",true);
+        capabilities.setCapability("autoAcceptAlerts", true);
         try {
-            appiumDriver = new AndroidDriver(new URL(getServerUrl()),capabilities);
+            appiumDriver = new AndroidDriver(new URL(getServerUrl()), capabilities);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        try{
-            Thread.sleep(2000);
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }
-        appiumDriver.switchTo().alert().accept();
-        try {
-            Thread.sleep(3000);
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }
-        appiumDriver.navigate().back();
-        try {
-            ((AndroidDriver)appiumDriver).pushFile("/Internal storage/Image.jpeg",imageFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        Map<String,Object> command = ImmutableMap.of("command","pm list packages");
+        String output = (String)appiumDriver.executeScript("mobile: shell",command);
+        System.out.println("--output is -"+output);
     }
 
     @AfterMethod
     public void tearDown(){
         stopServer();
     }
+
 }
