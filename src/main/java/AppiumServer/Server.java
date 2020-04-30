d405a2903bacd9c17c025cb2f4e486f0d8cdcf30
love <lovesoni607@gmail.com>
2020-04-30 22:37:13 +0530

    format server class

--- src/main/java/AppiumServer/Server.java
+++ src/main/java/AppiumServer/Server.java
@@ -3,24 +3,24 @@ package AppiumServer;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
-import io.appium.java_client.service.local.flags.ServerArgument;
 
 public class Server {
     private AppiumDriverLocalService appiumDriverLocalService;
 
-    public void startServer(){
+    public void startServer() {
         AppiumServiceBuilder appiumServiceBuilder = new AppiumServiceBuilder();
-        appiumServiceBuilder.withArgument(GeneralServerFlag.RELAXED_SECURITY);
+        appiumServiceBuilder.withArgument(GeneralServerFlag.RELAXED_SECURITY, "");
         appiumServiceBuilder.usingAnyFreePort();
         appiumServiceBuilder.withIPAddress("0.0.0.0");
         appiumDriverLocalService = AppiumDriverLocalService.buildService(appiumServiceBuilder);
         appiumDriverLocalService.start();
     }
 
-    public String getServerUrl(){
+    public String getServerUrl() {
         return appiumDriverLocalService.getUrl().toString();
     }
-    public void stopServer(){
+
+    public void stopServer() {
         appiumDriverLocalService.stop();
     }
 }
