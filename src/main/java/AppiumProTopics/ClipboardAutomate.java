55f2a021a17bac39dd644d99fe696546f5cc814d
love <lovesoni607@gmail.com>
2020-05-06 13:00:27 +0530

    stop server for clipboard method

--- src/main/java/AppiumProTopics/ClipboardAutomate.java
+++ src/main/java/AppiumProTopics/ClipboardAutomate.java
@@ -4,13 +4,17 @@ import AppiumServer.Server;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ClipboardAutomate extends Server {
-    private AppiumDriver appiumDriver;
+    private AppiumDriver driver;
+
+    private String ANDROID_APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.4.0/TheApp-v1.4.0.apk";
+
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         startServer();
     }
 
@@ -18,8 +22,15 @@ public class ClipboardAutomate extends Server {
     public void automateClipboard(){
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion","10.0");
         capabilities.setCapability("deviceName", "Android Emulator");
-        capabilities.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+        capabilities.setCapability("avd", "Pixel3");
+        capabilities.setCapability("app", ANDROID_APP);
         capabilities.setCapability("automationName", "UiAutomator2");
+    }
+
+    @AfterMethod
+    public void stop(){
+        startServer();
     }
 }
