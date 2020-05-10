927330e8db0e50508005942e3deb7c6b89f62e41
love <lovesoni607@gmail.com>
2020-05-10 22:33:08 +0530

    app path

--- src/main/java/AppiumProTopics/ClipboardAutomate.java
+++ src/main/java/AppiumProTopics/ClipboardAutomate.java
@@ -2,16 +2,19 @@ package AppiumProTopics;
 
 import AppiumServer.Server;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.clipboard.HasClipboard;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ClipboardAutomate extends Server {
     private AppiumDriver driver;
-
-    private String ANDROID_APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.4.0/TheApp-v1.4.0.apk";
 
     @BeforeMethod
     public void setUp() {
@@ -25,8 +28,16 @@ public class ClipboardAutomate extends Server {
         capabilities.setCapability("platformVersion","10.0");
         capabilities.setCapability("deviceName", "Android Emulator");
         capabilities.setCapability("avd", "Pixel3");
-        capabilities.setCapability("app", ANDROID_APP);
+        capabilities.setCapability("autoGrantPermissions",true);
+        capabilities.setCapability("app", "/Users/love/Desktop/AppiumPro/apps/TheApp.apk");
         capabilities.setCapability("automationName", "UiAutomator2");
+        try {
+            driver = new AndroidDriver(new URL(getServerUrl()),capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        ((HasClipboard)driver).setClipboardText("hiiiiiii");
+        System.out.println("Clipboard text msg is :"+((HasClipboard) driver).getClipboardText());
     }
 
     @AfterMethod
