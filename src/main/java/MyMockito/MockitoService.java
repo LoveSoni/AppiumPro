bbf13ac9f828e863ccd60639abeedc20d32d6937
love <lovesoni607@gmail.com>
2020-05-10 23:31:59 +0530

    assertion in mock class

--- src/main/java/MyMockito/MockitoService.java
+++ src/main/java/MyMockito/MockitoService.java
@@ -10,7 +10,7 @@ public class MockitoService {
     @Test
     public void myService(){
         ServiceProvider serviceObject = Mockito.mock(ServiceProvider.class);
-        Mockito.when(serviceObject.add(2,3)).thenReturn(7);
+        Mockito.when(serviceObject.add(2,3)).thenReturn(5);
         Assert.assertEquals(5,serviceObject.add(2,3));
     }
 }
