b4e323c73c54bfcd5d59165468b0fc6d8e7c7ce5
love <lovesoni607@gmail.com>
2020-05-10 23:58:50 +0530

    check value with mocking behaviour

--- src/main/java/MyMockito/MockInjection.java
+++ src/main/java/MyMockito/MockInjection.java
@@ -29,6 +29,13 @@ public class MockInjection {
         System.out.println(mockList.size());
 
         System.out.print(mockStudent.getNameList().get(0));
+
+        mockList.add(1,"new Member"); // if we will add value directly and will not def the behaviour(behaviour means like when and then)
+                // then with the mockStudent as well as for mockList object we will get null
+
+        System.out.println(mockList.get(1));
+
+        System.out.println(mockStudent.getNameList().get(1));
     }
 }
 
