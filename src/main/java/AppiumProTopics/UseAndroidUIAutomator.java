3e4e2b0a3851780227935e8defaa860d5d2d1dec
love <lovesoni607@gmail.com>
2020-05-10 22:33:37 +0530

    ui automate

--- src/main/java/AppiumProTopics/UseAndroidUIAutomator.java
+++ src/main/java/AppiumProTopics/UseAndroidUIAutomator.java
@@ -1,4 +1,40 @@
 package AppiumProTopics;
 
-public class UseAndroidUIAutomator {
+import AppiumServer.Server;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class UseAndroidUIAutomator extends Server {
+    private AppiumDriver driver;
+
+    @BeforeMethod
+    public void setDriver(){
+        startServer();
+    }
+
+    @Test
+    public void useAutomator(){
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"android");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"emulator-5554");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
+        desiredCapabilities.setCapability(AndroidMobileCapabilityType.AVD,"Pixel3");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP,System.getProperty("user.dir")+"/apps/ebay.apk");
+        try {
+            driver = new IOSDriver(new URL(getServerUrl()),desiredCapabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
