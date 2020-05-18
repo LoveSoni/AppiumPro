5ddb2956ad86b80c9e9189be615155826dacc22a
love <lovesoni607@gmail.com>
2020-05-18 23:11:18 +0530

    ios web

--- src/main/java/AppiumProTopics/IOSWeb.java
+++ src/main/java/AppiumProTopics/IOSWeb.java
@@ -5,6 +5,7 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.IOSMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -27,10 +28,14 @@ public class IOSWeb extends Server {
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Xs Max");
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12.2");
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "ios");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTO_WEBVIEW,true);
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "xcuitest");
+        Proxy proxy = new Proxy();
+        proxy.setHttpProxy("192.168.1.101");
+        desiredCapabilities.setCapability("remoteDebugProxy","127.0.0.1");
         desiredCapabilities.setCapability(IOSMobileCapabilityType.BROWSER_NAME, "safari");
         driver = new IOSDriver(new URL(getServerUrl()), desiredCapabilities);
-        driver.get("https://ww.google.com");
+        driver.get("https://www.capgemini.com");
     }
 
     @AfterMethod
