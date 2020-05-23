c72e1304770512d7a374c13904d80ee29123dd67
love <lovesoni607@gmail.com>
2020-05-23 23:12:55 +0530

    use link to navigate on login page

--- src/main/java/AppiumProTopics/DeepLink.java
+++ src/main/java/AppiumProTopics/DeepLink.java
@@ -1,4 +1,35 @@
 package AppiumProTopics;
 
-public class DeepLink {
+import AppiumServer.Server;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DeepLink extends Server {
+    private String APP_ANDROID = "/Users/love/Desktop/AppiumPro/apps/TheApp_DeepLink.apk";
+    private AndroidDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        startServer();
+    }
+
+    @Test
+    public void testDeepLink() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("deviceName", "Android Emulator");
+        caps.setCapability("app", APP_ANDROID);
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("udid", "emulator-5554");
+        driver = new AndroidDriver(new URL(getServerUrl()), caps);
+        String AUTH_USER = "alice";
+        String AUTH_PASS = "mypassword";
+        driver.get("theapp://login/alice/mypassword");
+    }
 }
