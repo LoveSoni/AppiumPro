5c64b3142f82d4b223f4fcdf299e372b1b98e935
love <lovesoni607@gmail.com>
2020-05-23 23:14:16 +0530

    sim sms on android

--- src/main/java/AppiumProTopics/SimulateSMS.java
+++ src/main/java/AppiumProTopics/SimulateSMS.java
@@ -1,10 +1,12 @@
 package AppiumProTopics;
 
 import AppiumServer.Server;
+import com.applitools.eyes.appium.Eyes;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -13,10 +15,12 @@ import java.net.URL;
 
 public class SimulateSMS extends Server {
     private AppiumDriver driver;
-
+    Eyes eyes;
     @BeforeMethod
     public void setUp(){
         startServer();
+        eyes = new Eyes();
+        eyes.setApiKey("WRm7YvJoRLQm3xQF5LTEh6LXKHPuA111Ya4wvwpGwxzFE110");
     }
 
     @Test
@@ -24,13 +28,22 @@ public class SimulateSMS extends Server {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "Android Emulator");
-        capabilities.setCapability(AndroidMobileCapabilityType.AVD,"Pixel3");
+        //capabilities.setCapability(AndroidMobileCapabilityType.AVD,"Pixel3");
+        capabilities.setCapability("udid","emulator-5554");
         capabilities.setCapability("automationName", "UiAutomator2");
         try {
             driver = new AndroidDriver(new URL(getServerUrl()),capabilities);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        ((AndroidDriver)driver).sendSMS("111","hey");
+        eyes.open(driver,"AppiumPro","SendNotifications");
+        eyes.checkWindow();
+      // ((AndroidDriver)driver).sendSMS("111","hey");
+    }
+    @AfterMethod
+    public  void testDown(){
+        eyes.close();
+        eyes.abortIfNotClosed();
+        driver.quit();
     }
 }
